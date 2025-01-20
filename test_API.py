0dc96a8446868e6f571a82a13cb9519f4a56cdbf
Tatiana Konischeva <moyzaldi@gmail.com>
2025-01-20 11:00:22 +0300

    add HW

--- test_API.py
+++ test_API.py
@@ -2,6 +2,7 @@ import json
 import os
 import requests
 from jsonschema import validate
+import allure
 
 url = 'https://reqres.in/api/users'
 job = "leader"
@@ -9,13 +10,13 @@ name = "morpheus"
 payload = {"name": job, "job": name}
 response = requests.post(url, data=payload)
 
-
+@allure.tag('Registration')
 def test_status_code_post():
     response = requests.post(url, data=payload)
 
     assert response.status_code == 201, f"Unexpected status code {response.status_code}"
 
-
+@allure.tag('Registration')
 def test_schema_validate():
     response = requests.post(url, data=payload)
 
@@ -23,7 +24,7 @@ def test_schema_validate():
     with open(os.path.abspath(os.path.join(os.path.dirname(__file__), f"./json_schemas/post_users.json"))) as file:
         validate(response.json(), schema=json.loads(file.read()))
 
-
+@allure.tag('Registration')
 def test_job_name_from_request_returns_in_body():
     response = requests.post(url, data=payload)
     body = response.json()
@@ -31,10 +32,9 @@ def test_job_name_from_request_returns_in_body():
     assert body["name"] == job, f"Unexpected name  - {body["name"]}"
     assert body["job"] == name, f"Unexpected job - {body["job"]}"
 
-
+@allure.tag('Registration')
 def test_status_code_get():
     response = requests.get(url, params={"page": 1, "per_page": 6})
-    data =  response.json(['data'])
-    set()
+
 
     assert response.status_code == 200, f"Unexpected status code {response.status_code}"
